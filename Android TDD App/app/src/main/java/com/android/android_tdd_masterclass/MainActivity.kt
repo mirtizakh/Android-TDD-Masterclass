237cf47a45107aa466f939d1ee57fef1d8ba027d
AzureAD\IrtizaKhursheed <irtizakh@gmail.com>
2022-03-01 10:27:40 +0500

    Addition - Coroutine example is added

--- Android TDD App/app/src/main/java/com/android/android_tdd_masterclass/MainActivity.kt
+++ Android TDD App/app/src/main/java/com/android/android_tdd_masterclass/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Globa
     }
 }
