2446ea337665d8c3027c8815e074f837ba890e07
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-12 08:29:42 +0500

    Addition - TDD coroutine example is completed

--- 3- Outside In TDD Kotlin Flow Example/app/src/test/java/com/example/outsideintddexample/unittests/CarShould.kt
+++ 3- Outside In TDD Kotlin Flow Example/app/src/test/java/com/example/outsideintddexample/unittests/CarShould.kt
@@ -6,14 +6,19 @@ import com.example.outsideintddexample.acceptancetests.MainCoroutineScopeRule
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
 
 class CarShould {
 
     private val engine: Engine = mock()
+
 
     private val car = Car(engine,5.0)
 
