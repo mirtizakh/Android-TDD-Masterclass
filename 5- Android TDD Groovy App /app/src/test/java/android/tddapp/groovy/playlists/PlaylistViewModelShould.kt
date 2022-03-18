4c077717809bc467ff79a48e75393ce85b12a1dd
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-19 02:24:07 +0500

    Addition - Viewmodel error case test case is added

--- 5- Android TDD Groovy App /app/src/test/java/android/tddapp/groovy/playlists/PlaylistViewModelShould.kt
+++ 5- Android TDD Groovy App /app/src/test/java/android/tddapp/groovy/playlists/PlaylistViewModelShould.kt
@@ -8,7 +8,6 @@ import io.mockk.spyk
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
-import org.junit.Before
 import org.junit.Test
 import petros.efthymiou.groovy.utils.getValueForTest
 
@@ -19,19 +18,20 @@ class PlaylistViewModelShould : BaseUnitTest() {
 
     private val mockPlayList = mockk<List<Playlist>>()
     private val expected = Result.success(mockPlayList)
+    private val exception = RuntimeException("Something went wrong")
 
-    @Before
-    fun setup() {
+    fun mockSuccessfulCase(): PlaylistViewModel {
         runBlocking {
             coEvery { mockRepository.getPlaylists() } returns flow {
                 emit(expected)
             }
         }
-        viewModel = PlaylistViewModel(mockRepository)
+        return PlaylistViewModel(mockRepository)
     }
 
     @Test
     fun getPlaylistsFromTheRepository() {
+        viewModel = mockSuccessfulCase()
         runBlocking {
             viewModel.playlists.getValueForTest()
 
@@ -42,8 +42,21 @@ class PlaylistViewModelShould : BaseUnitTest() {
 
     @Test
     fun emitsPlaylistsFromRepository() {
+        viewModel = mockSuccessfulCase()
         runBlocking {
             assertEquals(expected, viewModel.playlists.getValueForTest())
+        }
+    }
+
+    @Test
+    fun emitErrorWhenReceiveError() {
+        runBlocking {
+            coEvery { mockRepository.getPlaylists() } returns flow {
+                emit(Result.failure(exception))
+            }
+
+            viewModel = PlaylistViewModel(mockRepository)
+            assertEquals(exception, viewModel.playlists.getValueForTest()!!.exceptionOrNull())
         }
 
     }
