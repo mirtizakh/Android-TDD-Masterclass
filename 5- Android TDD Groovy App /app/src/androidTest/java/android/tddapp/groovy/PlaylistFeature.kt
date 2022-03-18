25a9148beaaa62ce3efe1da4e1366668deab6cbf
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-18 18:35:54 +0500

    Addition - 2nd acceptance test is added

--- 5- Android TDD Groovy App /app/src/androidTest/java/android/tddapp/groovy/PlaylistFeature.kt
+++ 5- Android TDD Groovy App /app/src/androidTest/java/android/tddapp/groovy/PlaylistFeature.kt
@@ -1,22 +1,84 @@
 package android.tddapp.groovy
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.adevinta.android.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
-
+import com.adevinta.android.barista.internal.matcher.DrawableMatcher.Companion.withDrawable
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Rule
+import java.util.EnumSet.allOf
 
 @RunWith(AndroidJUnit4::class)
 class PlaylistFeature {
 
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
-    @Rule get
+        @Rule get
 
+    // Check the title of MainActivity
     @Test
     fun displayScreenTitle() {
-       assertDisplayed("Playlist")
+        assertDisplayed("Playlists")
     }
+
+    // Check the items in recyclerview
+    @Test
+    fun displaysListOfPlaylists() {
+        assertRecyclerViewItemCount(R.id.playlists_list, 10)
+
+        onView(
+            allOf(
+                withId(R.id.playlist_name),
+                isDescendantOfA(nthChildOf(withId(R.id.playlists_list), 0))
+            )
+        )
+            .check(matches(withText("Hard Rock Cafe")))
+            .check(matches(isDisplayed()))
+
+        onView(
+            allOf(
+                withId(R.id.playlist_category),
+                isDescendantOfA(nthChildOf(withId(R.id.playlists_list), 0))
+            )
+        )
+            .check(matches(withText("rock")))
+            .check(matches(isDisplayed()))
+
+        onView(
+            allOf(
+                withId(R.id.playlist_image),
+                isDescendantOfA(nthChildOf(withId(R.id.playlists_list), 0))
+            )
+        )
+            .check(matches(withDrawable(R.mipmap.playlist)))
+            .check(matches(isDisplayed()))
+    }
+
+    fun nthChildOf(parentMatcher: Matcher<View>, childPosition: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("position $childPosition of parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                if (view.parent !is ViewGroup) return false
+                val parent = view.parent as ViewGroup
+
+                return (parentMatcher.matches(parent)
+                        && parent.childCount > childPosition
+                        && parent.getChildAt(childPosition) == view)
+            }
+        }
+    }
+
 }
