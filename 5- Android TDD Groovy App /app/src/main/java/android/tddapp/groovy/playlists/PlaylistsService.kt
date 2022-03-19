6597954fa185e12ec02f9458a6fb3d48444d5ed7
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-19 11:47:15 +0500

    Addition - Service class 3rd test case is added

--- 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/playlists/PlaylistsService.kt
+++ 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/playlists/PlaylistsService.kt
@@ -1,7 +1,9 @@
 package android.tddapp.groovy.playlists
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import java.lang.RuntimeException
 
 class PlaylistsService(
     private val playlistsAPI: PlaylistsAPI
@@ -9,6 +11,9 @@ class PlaylistsService(
     suspend fun fetchPlaylists(): Flow<Result<List<Playlists>>> {
 
         return flow { emit(Result.success(playlistsAPI.fetchAllPlaylists())) }
+            .catch {
+                emit(Result.failure(RuntimeException("Something went wrong")))
+        }
     }
 
 }
--- 5- Android TDD Groovy App /app/src/test/java/android/tddapp/groovy/playlists/PlaylistsServiceShould.kt
+++ 5- Android TDD Groovy App /app/src/test/java/android/tddapp/groovy/playlists/PlaylistsServiceShould.kt
@@ -7,9 +7,9 @@ import io.mockk.mockk
 import io.mockk.spyk
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import java.lang.RuntimeException
 
 class PlaylistsServiceShould : BaseUnitTest() {
 
@@ -20,7 +20,7 @@ class PlaylistsServiceShould : BaseUnitTest() {
     fun fetchPlaylistsFromAPI() {
         runBlocking {
             val service = PlaylistsService(mockPlaylistsAPI)
-            service.fetchPlaylists()
+            service.fetchPlaylists().first()
             coVerify(exactly = 1) { mockPlaylistsAPI.fetchAllPlaylists() }
         }
     }
@@ -31,6 +31,15 @@ class PlaylistsServiceShould : BaseUnitTest() {
             coEvery { mockPlaylistsAPI.fetchAllPlaylists() } returns mockPlaylists
             val service = PlaylistsService(mockPlaylistsAPI)
             assertEquals(Result.success(mockPlaylists),service.fetchPlaylists().first())
+        }
+    }
+
+    @Test
+    fun emitsErrorResultsWhenNetworkFails(){
+        runBlocking {
+            coEvery { mockPlaylistsAPI.fetchAllPlaylists() } throws RuntimeException("Network exception")
+            val service = PlaylistsService(mockPlaylistsAPI)
+            assertEquals("Something went wrong",service.fetchPlaylists().first().exceptionOrNull()!!.message)
         }
     }
 
