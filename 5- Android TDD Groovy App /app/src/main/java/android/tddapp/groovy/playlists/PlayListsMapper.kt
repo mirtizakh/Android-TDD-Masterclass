937b21526921ff2fb677a6a387bbbfe92442c268
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-21 01:16:38 +0500

    Addition - Mapper first test case is added

--- 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/playlists/PlayListsMapper.kt
+++ 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/playlists/PlayListsMapper.kt
@@ -3,7 +3,9 @@ package android.tddapp.groovy.playlists
 import javax.inject.Inject
 
 class PlayListsMapper @Inject constructor() : Function1<List<PlaylistsRaw>, List<Playlists>> {
-    override fun invoke(p1: List<PlaylistsRaw>): List<Playlists> {
-        return listOf()
+    override fun invoke(playlistsRaw: List<PlaylistsRaw>): List<Playlists> {
+        return playlistsRaw.map { value ->
+            Playlists(value.id, "", "", 1)
+        }
     }
 }
--- 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/playlists/PlaylistsRaw.kt
+++ 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/playlists/PlaylistsRaw.kt
@@ -1,3 +1,3 @@
 package android.tddapp.groovy.playlists
 
-class PlaylistsRaw
+class PlaylistsRaw(val id: String, val name: String, val category: String)
