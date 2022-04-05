23fe03c3f8983def0a4732ab7358cccb4f6c813e
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-04-05 17:20:38 +0500

    Addition - fragment added

--- 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/PlaylistsDetailsFragment.kt
+++ 5- Android TDD Groovy App /app/src/main/java/android/tddapp/groovy/PlaylistsDetailsFragment.kt
@@ -16,6 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [PlaylistsDetailsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class PlaylistsDetailsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
